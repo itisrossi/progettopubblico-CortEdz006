a8d6e6e676bb28fbf8573a66aa6f10f52811ae0d
CortEdz006 <124039641+CortEdz006@users.noreply.github.com>
2023-04-11 10:28:13 +0200

    Add files via upload

---
+++ C#/TestFrazioni/TestFrazioni/CHugeNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestFrazioni
+{
+    class CHugeNumber 
+    {
+        // 1. definisco variabili di istanza
+        private const int N = 300;
+        private int[] mCifre;
+
+        public int[] Cifre
+        {
+            get { return mCifre; }
+            set { mCifre = value; }
+        }
+
+        public CHugeNumber()
+        {
+            Cifre = new int[N];  
+        }
+
+        public CHugeNumber(string numero)
+        {
+            Cifre = new int[N];
+
+        }
+    }
+}
